bd9c460728b8b8f85347b6f3fef9b0853420a3c2
刘太全 <646154945@qq.com>
2019-06-26 16:05:20 +0800

    k8s-config

--- feign-hystrix/src/main/java/com/weilus/Test1Controller.java
+++ feign-hystrix/src/main/java/com/weilus/Test1Controller.java
@@ -4,14 +4,14 @@ import com.feign.clients.FeignClientService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
-import java.util.Enumeration;
 
 /**
  * Created by liutq on 2018/12/12.
@@ -25,18 +25,22 @@ public class Test1Controller {
     @Autowired
     FeignClientService service;
 
-    @RequestMapping("test/sayHello")
+    @Value("${test.name:马云}")
+    private String name;
+
+    @GetMapping("test/sayHello")
     public String sayHello(HttpServletRequest request){
-        Enumeration<String> enumeration = request.getHeaderNames();
-        while (enumeration.hasMoreElements()){
-            String headerName = enumeration.nextElement();
-            String header = request.getHeader(headerName);
-            LOGGER.info("feign-call header [{}] : {}",headerName,header);
-        }
-        return service.sayHello(Collections.singletonMap("name", "jhon"));
+        return name;
+//        Enumeration<String> enumeration = request.getHeaderNames();
+//        while (enumeration.hasMoreElements()){
+//            String headerName = enumeration.nextElement();
+//            String header = request.getHeader(headerName);
+//            LOGGER.info("feign-call header [{}] : {}",headerName,header);
+//        }
+//        return service.sayHello(Collections.singletonMap("name", "jhon"));
     }
 
-    @RequestMapping("test/hiMan")
+    @GetMapping("test/hiMan")
     public String hiMan(){
         return service.hiMan(Collections.singletonMap("name", "jhon"));
     }
