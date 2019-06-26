1e0882a20412f48aea6aca5abd83ddafc2ab9725
刘太全 <646154945@qq.com>
2019-06-26 15:21:34 +0800

    k8s-config

--- auth/data-mysql.sql
+++ auth/data-mysql.sql
@@ -1,6 +1,8 @@
 
 
 INSERT INTO users(username,password,enabled) VALUES ('liutaiq','{noop}123456',1);
+INSERT INTO authorities(username,authority) VALUES ('liutaiq','add');
+
 
 INSERT INTO oauth_client_details(client_id,client_secret,scope,authorized_grant_types,web_server_redirect_uri,autoapprove) VALUES
 ('acau','{noop}acausecret','user_info','authorization_code','http://aa.ccdd','true'),
