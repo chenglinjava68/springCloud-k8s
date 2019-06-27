10670a22c7ebe084fd03ad74709f28791e32a572
刘太全 <646154945@qq.com>
2019-06-27 15:48:24 +0800

    k8s-admin

--- admin/src/main/java/com/weilus/AdminApplication.java
+++ admin/src/main/java/com/weilus/AdminApplication.java
@@ -4,6 +4,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import spring.boot.admin.turbine.config.TurbineAutoConfiguration;
 
@@ -15,6 +16,7 @@ import spring.boot.admin.turbine.config.TurbineAutoConfiguration;
 @SpringBootApplication(exclude = TurbineAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableTurbine
+@EnableHystrixDashboard
 @EnableAdminServer
 public class AdminApplication {
 
