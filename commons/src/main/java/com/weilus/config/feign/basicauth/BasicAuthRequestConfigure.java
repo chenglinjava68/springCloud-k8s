c530a00bbabc88ee7f3ec5afc07c03814f978ef1
刘太全 <646154945@qq.com>
2019-06-27 09:27:22 +0800

    k8s-config

--- commons/src/main/java/com/weilus/config/feign/basicauth/BasicAuthRequestConfigure.java
+++ commons/src/main/java/com/weilus/config/feign/basicauth/BasicAuthRequestConfigure.java
@@ -1,19 +1,22 @@
-package com.feign.clients;
+package com.weilus.config.feign.basicauth;
 
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
-public class OauthClientConfig {
+public class BasicAuthRequestConfigure{
+
     @Bean
-    @ConditionalOnProperty(value = {"feign.client.config.oauth.username","feign.client.config.oauth.password"})
+    @RefreshScope
+    @ConditionalOnProperty(value = {"spring.security.user.username","spring.security.user.password"})
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor(
-            @Value("${feign.client.config.oauth.username}") String username,
-            @Value("${feign.client.config.oauth.password}") String password){
+            @Value("${spring.security.user.username}") String username,
+            @Value("${spring.security.user.password}") String password){
         return new BasicAuthRequestInterceptor(username,password);
     }
+
 }
