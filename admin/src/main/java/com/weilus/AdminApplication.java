781fe67dd318068bc16ee792d1108eb3d61e59c4
刘太全 <646154945@qq.com>
2019-06-27 16:20:35 +0800

    k8s-admin

--- admin/src/main/java/com/weilus/AdminApplication.java
+++ admin/src/main/java/com/weilus/AdminApplication.java
@@ -4,19 +4,16 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
-import spring.boot.admin.turbine.config.TurbineAutoConfiguration;
 
 /**
  * @author 1
  * @program springCloud
  * @date 2019-05-10 10:30
  **/
-@SpringBootApplication(exclude = TurbineAutoConfiguration.class)
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableTurbine
-@EnableHystrixDashboard
 @EnableAdminServer
 public class AdminApplication {
 
--- admin/src/main/java/com/weilus/SecuritySecureConfig.java
+++ admin/src/main/java/com/weilus/SecuritySecureConfig.java
@@ -30,9 +30,6 @@ public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(adminContextPath + "/assets/**").permitAll()
                 .antMatchers(adminContextPath + "/login").permitAll()
-
-                .mvcMatchers(adminContextPath + "/hystrix/**").permitAll()
-
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
