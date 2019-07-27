97f207129e58792b8992bc46fa6e03434d75310d
刘太全 <646154945@qq.com>
2019-07-27 09:50:06 +0800

    es

--- es/src/test/java/com/weilus/es/ESTest.java
+++ es/src/test/java/com/weilus/es/ESTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -44,34 +43,44 @@ public class ESTest extends AppTest{
     @Test
     public void testAdd(){
         List<Article> list = new ArrayList<>();
-        Article art = new Article();
-        art.setId(1L);
-        art.setTitle("中国商人在沙特唐人街开中餐饭店");
-        art.setPostTime(new Date());
-        art.setAuthor("admin");
-        art.setContent("这是内容。中国商人在沙特唐人街开中餐饭店");
-        list.add(art);
-        Article art1 = new Article();
-        art1.setId(2L);
-        art1.setTitle("沙特阿拉伯被汽车炸弹袭击");
-        art1.setPostTime(new Date());
-        art1.setAuthor("admin");
-        art1.setContent("沙特阿拉伯被汽车炸弹袭击。。。。这是一篇文章");
-        list.add(art1);
-        Article art2 = new Article();
-        art2.setId(3L);
-        art2.setTitle("美国总统访问阿拉斯加州");
-        art2.setPostTime(new Date());
-        art2.setAuthor("admin");
-        art2.setContent("美国总统访问阿拉斯加州。。。。这是一篇文章");
-        list.add(art2);
+//        Article art = new Article();
+//        art.setId(1L);
+//        art.setTitle("中国商人在沙特唐人街开中餐饭店");
+//        art.setPostTime(new Date());
+//        art.setAuthor("admin");
+//        art.setContent("这是内容。中国商人在沙特唐人街开中餐饭店");
+//        list.add(art);
+
+//        Article art1 = new Article();
+//        art1.setId(2L);
+//        art1.setTitle("沙特阿拉伯被汽车炸弹袭击");
+//        art1.setPostTime(new Date());
+//        art1.setAuthor("admin");
+//        art1.setContent("沙特阿拉伯被汽车炸弹袭击。。。。这是一篇文章");
+//        list.add(art1);
+
+//        Article art2 = new Article();
+//        art2.setId(3L);
+//        art2.setTitle("美国总统访问阿拉斯加州");
+//        art2.setPostTime(new Date());
+//        art2.setAuthor("admin");
+//        art2.setContent("美国总统访问阿拉斯加州。。。。这是一篇文章");
+//        list.add(art2);
+
+//        Article art3 = new Article();
+//        art3.setId(4L);
+//        art3.setTitle("张学友在美国唐人街开餐馆");
+//        art3.setPostTime(new Date());
+//        art3.setAuthor("admin");
+//        art3.setContent("明星开餐馆了");
+//        list.add(art3);
+
 //        repository.saveAll(list);
-        repository.save(art);
     }
 
     @Test
     public void testsearch(){
-        Iterable<Article> iterable = repository.search(QueryBuilders.matchQuery("title","沙特"));
+        Iterable<Article> iterable = repository.search(QueryBuilders.matchQuery("title","唐人街"));
         Iterator<Article> it= iterable.iterator();
         while (it.hasNext()){
             System.out.println(it.next());
